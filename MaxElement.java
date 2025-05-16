eb881d6a970a399eb6e872bf278c231ece6cb5e3
Gokul T <131269675+Gokul049@users.noreply.github.com>
2025-05-16 08:42:50 +0530

    basic java Problem

--- MaxElement.java
+++ MaxElement.java
@@ -3,14 +3,14 @@ package ArraysProblems;
 public class MaxElement {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int arr[] = {1,765,36,36,54,75,345,346,753,252,3642,836,2626,2,22662,4562};
-		int max = arr[0];
+		
+		int arr[] = {1,765,36,36,54,75,345,346,753,252,3642,836,2626,2,22662,4562}; // array initialization
+		int max = arr[0]; // set first element of array in max
 		
 		for(int ind=1;ind<arr.length;ind++)
 		{
-			if(arr[ind]>max)
-				max = arr[ind];
+			if(arr[ind]>max) 
+				max = arr[ind]; 
 		}
 		System.out.printf("Max Element in the Array : %d",max);
 	}
